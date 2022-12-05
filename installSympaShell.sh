82fd4794c37781c59e7194609a274b5e9bf26075
valentin <valentin.raillard@etu.u-bordeaux.fr>
2022-12-05 22:55:47 +0100

    v1.2.1

--- installSympaShell.sh
+++ installSympaShell.sh
@@ -1,29 +1,35 @@
 # Fonts Download / Install
-echo "Downloading the fonts"
-wget -q --show-progress https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
-FONTDIR=~/.fonts
-if [ -d "$FONTDIR" ]
+echo "Checking the fonts..."
+if (( $(fc-list | grep "JetBrains Mono" | wc -l) < 100 ))
 then
-    echo "$FONTDIR already exist. Processing..."
-else
-    echo "$FONTDIR don't exist. One will be created"
-    mkdir $FONTDIR
-fi
-echo "Unzipping the fonts in $FONTDIR"
-unzip -q JetBrainsMono.zip -d $FONTDIR
-echo "Installing the fonts"
-fc-cache -f
+    echo "fonts not installed. Downloading the fonts"
+    wget -q --show-progress https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
+    FONTDIR=~/.fonts
+    if [ -d "$FONTDIR" ]
+    then
+        echo "$FONTDIR already exist. Processing..."
+    else
+        echo "$FONTDIR don't exist. One will be created"
+        mkdir $FONTDIR
+    fi
+    echo "Unzipping the fonts in $FONTDIR"
+    unzip -q JetBrainsMono.zip -d $FONTDIR
+    echo "Installing the fonts"
+    fc-cache -f
 
-if [ $(fc-list | grep "JetBrains Mono" | wc -l) -gt 100 ]
-then
-    echo "Fonts correctly installed"
-else
-    echo "Fonts install failed"
-fi
+    if [ $(fc-list | grep "JetBrains Mono" | wc -l) -gt 100 ]
+    then
+        echo "Fonts correctly installed"
+    else
+        echo "Fonts install failed"
+    fi
 
-echo "Suppresing the remaining files"
-rm JetBrainsMono*.zip
-echo "Remaining files supressed"
+    echo "Suppresing the remaining files"
+    rm JetBrainsMono*.zip
+    echo "Remaining files supressed"
+else
+    echo "fonts already installed skip to install process"
+fi
 
 # Script Install
 SCRIPTDIR=~/.sympa-shell
@@ -36,6 +42,12 @@ echo "$SCRIPTDIR will be created"
 mkdir $SCRIPTDIR
 echo "Copying files to $SCRIPTDIR"
 echo "$(cat sympaShellBashrc)" >> $SCRIPTDIR/sympa-shell.sh
-echo "" >> ~/.bashrc
-echo "source $SCRIPTDIR/sympa-shell.sh" >> ~/.bashrc
+if (( $(grep "source $SCRIPTDIR/sympa-shell.sh" ~/.bashrc | wc -l) < 1 ))
+then
+    echo "Adding the link to sympa-shell.sh in .bashrc"
+    echo "" >> ~/.bashrc
+    echo "source $SCRIPTDIR/sympa-shell.sh" >> ~/.bashrc
+else
+    echo "Link between sympa-shell.sh and .bashrc already exist"
+fi
 echo "Installation complete. Please restart your shell to see your SYMPA SHELL !"
